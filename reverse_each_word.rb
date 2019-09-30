ba1b7a7bb7ba4c4bc3f43a8ce4dce0be31eba083
Rachid Rakaa <rachid1982fsb@gmail.com>
2019-09-30 19:51:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word (string)
   array= string.split(/ /)
   reverse_array=[]
   array.each {|word| reverse_array.push(word.reverse)}
-  reverse_array
+  reverse_array.join(' ')
 end
 
 
